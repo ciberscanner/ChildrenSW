3b9dc8cf1b66533380c7861d6b52079233e08cba
ciberscanner <ciberscanner@gmail.com>
2016-06-21 03:32:52 -0500

    Paso a paso
    nuevas vistas y pedacitos de cambio en código jejeje

--- src/com/ciberscanner/app/childrensw/Denuncia.java
+++ src/com/ciberscanner/app/childrensw/Denuncia.java
@@ -3,24 +3,29 @@ package com.ciberscanner.app.childrensw;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Denuncia extends Activity {
 	// --------------------------------------------------------------------
 	// Variables
-	
+	private TextView titulo;
+
 	// --------------------------------------------------------------------
 	// Constructor
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_estadistica);
+		setContentView(R.layout.activity_denuncia);
+		titulo = (TextView) findViewById(R.id.txt_title);
+		titulo.setText("Denunciar");
 	}
 	// --------------------------------------------------------------------
 	//
 
 	// --------------------------------------------------------------------
-		//
-		public void back(View v) {
-			finish();
-		}
+	//
+	public void back(View v) {
+		finish();
+	}
+	
 }
--- src/com/ciberscanner/app/childrensw/Registro.java
+++ src/com/ciberscanner/app/childrensw/Registro.java
@@ -3,17 +3,23 @@ package com.ciberscanner.app.childrensw;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Registro extends Activity {
 	// --------------------------------------------------------------------
 	// Variables
+	private TextView titulo;
+
 	
 	// --------------------------------------------------------------------
 	// Constructor
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_inicio);
+		setContentView(R.layout.activity_registro);
+		
+		titulo=(TextView)findViewById(R.id.txt_title);
+		titulo.setText("Registro");
 	}
 
 	// --------------------------------------------------------------------
